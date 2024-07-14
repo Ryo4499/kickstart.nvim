7f8cf0609b1e79c71584334891ecd989f9fd4316
Ryo4499 <ryo.p4499@gmail.com>
2024-07-14 09:29:04 +0900

    update: modular nvim

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,18 +15,6 @@ return {
   },
   opts = {
     filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_by_name = {
-          '.git',
-          '.DS_Store',
-          'thumbs.db',
-        },
-        never_show = {},
-      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
